f392305069074976667ff00847cae0a1077c8f12
Luis Garcia Figueres <garciafigueres@gmail.com>
2026-04-19 08:49:08 +0200

    Initial commit

---
+++ verbs.js
@@ -0,0 +1,84 @@
+const VERBS_RAW = `
+1,be,was/were,been,ser / estar
+2,become,became,become,llegar a ser
+3,begin,began,begun,empezar / comenzar
+4,bite,bit,bitten,morder
+5,break,broke,broken,romper
+6,bring,brought,brought,traer / llevar
+7,build,built,built,construir
+8,burn,burnt/burned,burnt/burned,quemar / arder
+9,buy,bought,bought,comprar
+10,can,could,-,poder
+11,catch,caught,caught,coger / atrapar
+12,choose,chose,chosen,elegir / escoger
+13,come,came,come,venir / llegar
+14,cost,cost,cost,costar
+15,cut,cut,cut,cortar
+16,do,did,done,hacer
+17,draw,drew,drawn,dibujar
+18,dream,dreamt,dreamt,soñar
+19,drink,drank,drunk,beber
+20,drive,drove,driven,conducir
+21,eat,ate,eaten,comer
+22,fall,fell,fallen,caerse
+23,feed,fed,fed,alimentar
+24,feel,felt,felt,sentir
+25,fight,fought,fought,luchar
+26,find,found,found,encontrar
+27,fly,flew,flown,volar
+28,forget,forgot,forgotten,olvidar
+29,freeze,froze,frozen,helar / congelar
+30,get,got,got,conseguir / obtener
+31,give,gave,given,dar
+32,go,went,gone,ir
+33,grow,grew,grown,crecer / cultivar
+34,have,had,had,tener / haber
+35,hear,heard,heard,oír
+36,hide,hid,hidden,esconder
+37,hit,hit,hit,golpear / chocar
+38,hold,held,held,sostener / aguantar
+39,hurt,hurt,hurt,herir / doler
+40,keep,kept,kept,conservar / mantener
+41,know,knew,known,saber / conocer
+42,learn,learnt/learned,learnt/learned,aprender
+43,leave,left,left,dejar (abandonar)
+44,lend,lent,lent,prestar
+45,let,let,let,dejar (permitir)
+46,lose,lost,lost,perder
+47,make,made,made,hacer / fabricar
+48,mean,meant,meant,significar
+49,meet,met,met,encontrarse / quedar con
+50,pay,paid,paid,pagar
+51,put,put,put,poner / colocar
+52,read,read,read,leer
+53,ride,rode,ridden,montar
+54,ring,rang,rung,sonar
+55,run,ran,run,correr
+56,say,said,said,decir
+57,see,saw,seen,ver
+58,sell,sold,sold,vender
+59,send,sent,sent,enviar
+60,shine,shone,shone,brillar
+61,shoot,shot,shot,disparar
+62,shut,shut,shut,cerrar (fuertemente)
+63,sing,sang,sung,cantar
+64,sit,sat,sat,sentarse
+65,sleep,slept,slept,dormir
+66,speak,spoke,spoken,hablar
+67,spell,spelt,spelt,deletrear
+68,spend,spent,spent,gastar
+69,stand,stood,stood,estar de pie / colocar
+70,steal,stole,stolen,robar
+71,swim,swam,swum,nadar
+72,swing,swung,swung,balancearse
+73,take,took,taken,llevarse / coger
+74,teach,taught,taught,enseñar
+75,tell,told,told,decir (contar)
+76,think,thought,thought,pensar
+77,throw,threw,thrown,arrojar / lanzar
+78,understand,understood,understood,entender / comprender
+79,wake,woke,woken,despertar
+80,wear,wore,worn,llevar puesto
+81,win,won,won,ganar
+82,write,wrote,written,escribir
+`;
